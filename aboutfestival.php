1f39de8863d77fe4f605b4ba32c88802dac2fbc2
Oltiana <oltianabalaj@gmail.com>
2025-01-11 20:10:04 +0100

    Create aboutfestival.php

---
+++ aboutfestival.php
@@ -0,0 +1,30 @@
+<?php
+$pageTitle = "About Festival";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $pageTitle?></title>
+</head>
+<body>
+    <header> 
+        <div class = "logo">
+            <img src = "images/pintlogo.webp" alt= "Pint Festival Logo">
+            <span>PINT FESTIVAL</span>
+        </div>
+        <ul class = "nav-links">
+            <li><a href= "Home.php">Home</a></li>
+            <li><a href= "#" class = "active">About Festival</a></li>
+            <li><a href= "aboutus.php">About Us</a></li>
+            <li><a href= "tickets.php">Tickets</a></li>
+            <li><a href= "#">Merchandise</a></li>
+            <li><a href= "#">Faq</a></li>
+            <li><a href= "#">News</a></li>
+            <li><a href= "login.php">Login</a></li>
+        </ul>
+    </header>
+</body>
+</html>
