5c4910fae52e322d6efe625ba3de2207c17e3971
Oltiana <oltianabalaj@gmail.com>
2025-01-19 23:42:43 +0100

    php

---
+++ checkout.php
@@ -0,0 +1,32 @@
+<?php
+$pageTitle = "Checkout";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/checkout.css">
+    <title><?php echo $pageTitle; ?></title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="images/pintlogo.webp" alt="Pint Festival Logo">
+            <span>PINT FESTIVAL</span>
+        </div>
+        <ul class="nav-links">
+            <li><a href="Home.php">Home</a></li>
+            <li><a href="aboutfestival.php">About Festival</a></li>
+            <li><a href="aboutus.php">About Us</a></li>
+            <li><a href="Tickets.php">Tickets</a></li>
+            <li><a href="#">Merchandise</a></li>
+            <li><a href="#">Faq</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="login.php">Login</a></li>
+        </ul>
+    </header>
+    
+</body>
+</html>
