29100c94bdead46cecd188a4c7fd50598b284235
Loreta-Bl <loretabilalli8@gmail.com>
2025-01-29 00:44:46 -0800

    Update Merchandise.php

--- Merchandise.php
+++ Merchandise.php
@@ -77,7 +77,7 @@ if (isset($_POST['buy_now'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PINT Festival - Merchandise</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="merchandise.css">
+    <link rel="stylesheet" href="Merchandise.css">
 </head>
 <body>
     <header>
