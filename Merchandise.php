9ff4030f49a4f67d276d2f1e18f6ce0007bd4f02
Loreta-Bl <loretabilalli8@gmail.com>
2025-01-31 13:03:16 -0800

    Update Merchandise.php

--- Merchandise.php
+++ Merchandise.php
@@ -53,7 +53,7 @@ $result = $connection->query($sql);
                         while ($row = $result->fetch_assoc()) {
                             echo '<div class="col-md-4 mb-4">';
                             echo '<div class="card h-100">';
-                            echo '<img src="images/' . htmlspecialchars($row['image']) . '" class="card-img-top" alt="' . htmlspecialchars($row['title']) . '">';
+                            echo '<img src="Images/' . htmlspecialchars($row['image']) . '" class="card-img-top" alt="' . htmlspecialchars($row['title']) . '">';
                             echo '<div class="card-body">';
                             echo '<h5 class="card-title">' . htmlspecialchars($row['title']) . '</h5>';
                             echo '<p class="card-text">' . htmlspecialchars($row['description']) . '</p>';
