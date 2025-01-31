d7a93516a66d5614506f771b3a71c0f768417e94
Oltiana <oltianabalaj@gmail.com>
2025-01-31 13:22:41 +0100

    Update AdminDashboard.php

--- AdminDashboard.php
+++ AdminDashboard.php
@@ -12,6 +12,7 @@ if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/AdminDashboard.css">
     <title>Admin Dashboard</title>
     </head>
 <body>
