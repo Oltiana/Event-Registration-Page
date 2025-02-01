19fc72362133ae960873e62659138eae08dfd200
Oltiana <oltianabalaj@gmail.com>
2025-02-01 19:12:54 +0100

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -7,7 +7,7 @@ $pageTitle = "Checkout";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="CSS/checkout.css">
+    <link rel="stylesheet" href="checkout.css">
     <title><?php echo $pageTitle; ?></title>
     
     <script>
