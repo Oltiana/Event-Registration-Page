df21ce5579214bae8399c0cd9a8936b8d420a7c4
adeanimanaj <adeanimanajjj@gmail.com>
2025-02-02 21:19:00 +0100

    Update Your_Purchase.php

--- Your_Purchase.php
+++ Your_Purchase.php
@@ -19,7 +19,7 @@ if (!empty($_SESSION['cart'])) {
 <body>
     <header>
         <div class="logo">
-            <img src="pintlogo.webp" alt="Pint Festival Logo">
+            <img src="images/pintlogo.webp" alt="Pint Festival Logo">
             <span>PINT FESTIVAL</span>
         </div>
         <ul class="nav-links">
