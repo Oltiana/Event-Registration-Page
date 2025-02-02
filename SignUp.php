2b8668db656cc5670de0d1bec17f5b34df1926b3
Loreta-Bl <loretabilalli8@gmail.com>
2025-02-01 22:21:41 -0800

    c

--- SignUp.php
+++ SignUp.php
@@ -57,7 +57,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register Form</title>
-    <link rel="stylesheet" href="CSS/Register.css">
+    <link rel="stylesheet" href="CSS/SignUp.css">
 </head>
 <body>
 
